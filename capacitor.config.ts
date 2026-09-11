39c2f15339424b2b9fb75e2c865618bc88c63477
Ali Muntaha <alimuntaha08@gmail.com>
2026-09-11 08:30:04 +0700

    Add Capacitor Android platform

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.motopos.app',
+  appName: 'motopos',
+  webDir: 'dist'
+};
+
+export default config;
